5e1ad1ba59b8c50144aaa7957a5a7214170e0c3c
hammondgorman99 <hammondgorman99@gmail.com>
2021-06-24 10:35:19 +0000

    add IMemoryDatabase

--- Hubtel.eCommerce.Cart.Api/Startup.cs
+++ Hubtel.eCommerce.Cart.Api/Startup.cs
@@ -21,7 +21,7 @@ namespace Hubtel.eCommerce.Cart.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CartDbContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:CartDb"]));
+            services.AddDbContext<CartDbContext>(opts => opts.UseInMemoryDatabase(databaseName:"CartDB"));
             services.AddControllers();
 
             services.AddSwaggerGen( c => c.SwaggerDoc("v1", new OpenApiInfo { Title ="CartApi", Version="v1" }) );
